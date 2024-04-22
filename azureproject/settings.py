1104f6ce506679ddd676938676fc4afabdd1599b
Gevorg Vardanyan <35601415+gevorg-vardanyan-im@users.noreply.github.com>
2024-04-22 12:47:29 +0400

    Update settings.py

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('ae3b7d0ae6e707fcc9d94099f004cb4dc5e4470f07ce866d8f9fa671ba45107e')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
